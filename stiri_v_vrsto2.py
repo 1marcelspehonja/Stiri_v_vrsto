03d99644663b436179aaacde4d239ab74652e8d1
Marcel Spehonja <spehonja.marcel@gmail.com>
2017-09-13 16:56:09 +0200

    popravek

--- stiri_v_vrsto2.py
+++ stiri_v_vrsto2.py
@@ -14,11 +14,12 @@ class Stiri_v_vrsto:
         self.gumbi = []
         
         for vrstica in range(self.plosca.visina):
+            
             vrstica_gumbov = []
             for stolpec in range(self.plosca.dolzina):
                 def dodaj_zeton(vrstica=vrstica, stolpec=stolpec):
-                    self.dodaj_zeton2(stolpec, vrstica)
-                    self.potek_igre()
+                    
+                    self.potek_igre2(stolpec, vrstica)
                 gumb = tk.Button(prikaz_plosce, text=' ',height=2, width=4,
                                  command=dodaj_zeton)
                 gumb.grid(row=vrstica, column=stolpec)
@@ -26,7 +27,7 @@ class Stiri_v_vrsto:
             self.gumbi.append(vrstica_gumbov)
         prikaz_plosce.pack()
 
-        self.navodila = tk.Label(okno, text='navodila')
+        self.navodila = tk.Label(okno)
         self.navodila.pack()
         
         gumb_izhod = tk.Button(okno, text='Izhod', fg="brown", command=okno.destroy)
@@ -47,8 +48,8 @@ class Stiri_v_vrsto:
         elif self.plosca.kdaj_je_plosca_polna() == True:
             self.navodila.config(text='Igra je izenačena!')
 
-    def potek_igre(self):
-        
+
+    def potek_igre2(self, stolpec, vrstica):   
         zmaga_prvi = self.plosca.zmaga('x')
         zmaga_drugi = self.plosca.zmaga('y')
         self.zmaga = False
@@ -56,48 +57,57 @@ class Stiri_v_vrsto:
             igralec_ena = True
             while (igralec_ena == True):
                 self.navodila.config(text='Igralec 1, izberi stolpec')
-                a = self.plosca.poteza(1, 'x')
-                if self.plosca.omejitev(a):
-                    self.plosca.poteza(a, 'x')
-                    igralec_ena = False
-                else:
-                    self.navodila.config(text='Ta stolpec je že poln!')
+                
+                self.dodaj_zeton2(stolpec, vrstica, 'x')
+                igralec_ena = False
+                
         
             if zmaga_prvi ==  True:
                 self.navodila.config(text='Igralec 1 je zmagal!')
                 self.zmaga = True
                 break
-            if self.plosca.kdaj_je_plosca_polna() == True:
+            elif self.plosca.kdaj_je_plosca_polna() == True:
                 self.navodila.config(text='Igra je izenačena!')
                 self.zmaga = True
                 break
-            igralec_dva = True
-            while (igralec_dva == True):
-                self.navodila.config(text='Igralec 2, izberi stolpec')
+            else:
+                
+                igralec_dva = True
+                while (igralec_dva == True):
+                    self.navodila.config(text='Igralec 2, izberi stolpec')
+                    self.dodaj_zeton2(stolpec, vrstica, 'y')
+                    igralec_dva = False
+                    break
                 if zmaga_drugi == True:
                     self.navodila.config(text='Igralec 2 je zmagal!')
                     self.zmaga = True
                     break
-                if self.plosca.kdaj_je_plosca_polna() == True:
+                elif self.plosca.kdaj_je_plosca_polna() == True:
                     self.navodila.config(text='Igra je izenačena!')
                     self.zmaga = True
+                else:
                     break
 
-            
+
+        #if self.plosca.stevilo_zetonov_v_stolpcu(stolpec) != 6:
+              #      self.plosca.poteza(stolpec, 'x')
+              #      igralec_ena = False
+             #   else:
+              #      self.navodila.config(text='Ta stolpec je že poln!')
 
     def izrisi(self):
         print(self.plosca)
 
     
         
-    def dodaj_zeton2(self, stolpec, vrstica):
-        rezultat = self.plosca.poteza(stolpec, 'x')
-        self.gumbi[6 - (self.plosca.stevilo_zetonov_v_stolpcu(stolpec))][stolpec].config(text='x')
+    def dodaj_zeton2(self, stolpec, vrstica, zeton):
+        rezultat = self.plosca.poteza(stolpec, zeton)
+        self.gumbi[6 - (self.plosca.stevilo_zetonov_v_stolpcu(stolpec))][stolpec].config(text=zeton)
         for vrstica in range(self.plosca.visina):
             if self.gumbi[vrstica][stolpec] != '':
-                self.gumbi[vrstica - 1][stolpec] = 'x'
+                self.gumbi[vrstica - 1][stolpec] = zeton
                 return
-        self.gumbi[self.plosca.visina - 1][stolpec] = 'x'
+        self.gumbi[self.plosca.visina - 1][stolpec] = zeton
         
     
 okno = tk.Tk()
